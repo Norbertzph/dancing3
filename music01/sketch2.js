ada0e6a2fe06a3e7b7a8c9d8b6da59482d792eb7
norbertzph <841121049z@gmail.com>
2018-10-03 13:39:26 -0400

    bouncing ball

--- music01/sketch2.js
+++ music01/sketch2.js
@@ -5,52 +5,45 @@ var overBox = false;
 var locked = false;
 var xOffset = 0.0; 
 var yOffset = 0.0; 
-var col = 0;
+var x = 0;
+var speed = 3;
+
 var song;
 
-
 function preload() {
-  
   song = loadSound("music01.mp3");
 }
 
 function setup() {
     
   getAudioContext().resume();
-  createCanvas(600, 400);
+    
+  createCanvas(720, 400);
   bx = width/2.0;
   by = height/2.0;
   rectMode(RADIUS);
   strokeWeight(0);
+    
+  // playing the music
+  song.setVolume(0.1);
+  song.play();
 }
 
 function draw() { 
-  col = mouseX;
-  background(col);
-  
-  // Test if the cursor is over the box 
-  if (mouseX > bx-boxSize && mouseX < bx+boxSize && 
-      mouseY > by-boxSize && mouseY < by+boxSize) {
-    overBox = true;  
-     
-  } else {
-    stroke(156,39,176);
-    fill(244,122,158);
-    overBox = false;
-  }
-  
+  background(237,34,93);
+
   // Draw the box
-  rect(mouseX, by, boxSize, boxSize);
+  rect(x, by, boxSize, boxSize);
     
-    if(mouseX < 200){
-    console.log(song.isPlaying);
-    if (song.isPlaying() == 0) 
-    {
-   song.play();
-    }
-  }else{
-    song.pause();
-  }
+        x = x + speed;
+    
+        if (x > width){
+            speed = -3;  
+}
+            if (x < 0){
+            speed = 3;  
+}
+
 }
 
 function mousePressed() {
@@ -75,3 +68,4 @@ function mouseDragged() {
 function mouseReleased() {
   locked = false;
 }
+
