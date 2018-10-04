8f59acb01aabcfc23a6343e2067ffc130ca25d51
norbertzph <841121049z@gmail.com>
2018-10-04 17:05:20 -0400

    Add the sprites

--- first/sketch.js
+++ first/sketch.js
@@ -1,5 +1,10 @@
+var img;
 
 function setup() 
+createCanvas(windowWidth,widowHeight);
+
+img = loadImage("first/purple.png");
+
 
 function draw() { 
   background(51);
--- music01/sketch.js
+++ music01/sketch.js
@@ -68,4 +68,3 @@ function mouseDragged() {
 function mouseReleased() {
   locked = false;
 }
-
