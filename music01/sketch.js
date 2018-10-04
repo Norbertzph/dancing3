b125ba197aed010e5b4141da147d0579498ebcbc
norbertzph <841121049z@gmail.com>
2018-10-03 23:59:24 -0400

    music play

--- music01/sketch.js
+++ music01/sketch.js
@@ -7,26 +7,26 @@ var xOffset = 0.0;
 var yOffset = 0.0; 
 var x = 0;
 var speed = 3;
-
 var song;
+
 
 function preload() {
   song = loadSound("music01.mp3");
 }
 
 function setup() {
-    
   getAudioContext().resume();
-    
+
   createCanvas(720, 400);
   bx = width/2.0;
   by = height/2.0;
   rectMode(RADIUS);
   strokeWeight(0);
     
-        // playing the music
-  song.setVolume(0.1);
   song.play();
+  song.loop(); 
+  song.setVolume(0.5);
+    
 }
 
 function draw() { 
