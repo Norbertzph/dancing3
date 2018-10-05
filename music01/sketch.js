c85cfa7f737af321ad7f40f8e0681728ec54dc31
norbertzph <841121049z@gmail.com>
2018-10-04 21:22:33 -0400

    Go back.....

--- music01/sketch.js
+++ music01/sketch.js
@@ -1,6 +1,12 @@
 var bx;
 var by;
 var boxSize = 75;
+var overBox = false;
+var locked = false;
+var xOffset = 0.0; 
+var yOffset = 0.0; 
+var x = 0;
+var y = 0;
 var speed = 3;
 var song;
 var nSpeed,pSpeed;
@@ -10,76 +16,52 @@ function preload() {
 }
 
 function setup() {
-    // Position Variables
-    x = 0;
-    y = 0;
- 
-// Speed - Velocity
-    vx = 0;
-    vy = 0;
- 
-// Acceleration
-    ax = 0;
-    ay = 0;
-
-    vMultiplier = 0.1;
-    bMultiplier = 0.6;
-    
   getAudioContext().resume();
 
   createCanvas(displayWidth, displayHeight);
 
-  bx = 0;
-  by = 0;
+  bx = width/2.0;
+  by = height/2.0;
   rectMode(RADIUS);
   strokeWeight(0);
    
   nSpeed = -3;
   pSpeed = 3;
-    
 }
 
 function draw() { 
   background(237,34,93);
     
+
+
     
   // Draw the box
   rect(x, y, boxSize, boxSize);
-
+    
+        x = x + speed;
+        y = y + speed;
+    
+        if (x > width){
+            speed = nSpeed;  
+}
+            if (x < 0){
+            speed = pSpeed;  
 }
 
-function rectMove() {
-
-	ax = accelerationX;
-	ay = accelerationY;
-
-	vx = vx + ay;
-	vy = vy + ax;
-
-	// Bounce when touch the edge of the canvas
-	if (x < 0) { 
-		x = 0; 
-		vx = -vx * bMultiplier; 
-	}
- 	if (y < 0) { 
- 		y = 0; 
- 		vy = -vy * bMultiplier; 
- 	}
- 	if (x > displayWidth - 20) { 
- 		x = displayWidth - 20; 
- 		vx = -vx * bMultiplier; 
- 	}
- 	if (y > displayHeight - 20) { 
- 		y = displayHeight - 20; 
- 		vy = -vy * bMultiplier; 
- 	}
+            if (y > width){
+            speed = nSpeed;  
+}
+            if (y < 0){
+            speed = pSpeed;  
+}
+    
 }
 
 function deviceShaken(){
     if (song.isPlaying() == 0){
         song.play();
-        y = y + vy * vMultiplier; 
-	    x = x + vx * vMultiplier;
     }
-
+    nSpeed=-10;
+    pSpeed=10;
+    
 }
